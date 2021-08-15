4d7c8670e4d6819b75f67ff6553f524ccb7d4689
Амурский Андрей <amupckuu@yandex.ru>
2021-08-15 11:28:32 +0300

    исравил cars_in_bd.py

--- cars_in_bd.py
+++ cars_in_bd.py
@@ -1,25 +1,25 @@
 import sqlite3
 
 
-def add_car_in_bd(db_cars, date_car):
+def add_car_in_bd(db_cars, car_info):
     with sqlite3.connect(db_cars) as db_cars_conn:
         cursor = db_cars_conn.cursor()
         cursor.execute("""INSERT INTO cars(model, color, price) 
-            VALUES (?, ?, ?)""", date_car)
+            VALUES (?, ?, ?)""", car_info)
         db_cars_conn.commit()
 
 
 def create_bd(db_name):
     try:
-        with sqlite3.connect(db_name) as sqlite_connection:
-            cursor = sqlite_connection.cursor()
+        with sqlite3.connect(db_name) as db_connection:
+            cursor = db_connection.cursor()
             cursor.execute("""CREATE TABLE IF NOT EXISTS cars(
                 car_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 model TEXT NOT NULL,
                 color TEXT NOT NULL,
                 price INT NOT NULL)
             """)
-            sqlite_connection.commit()
+            db_connection.commit()
 
     except sqlite3.Error as error:
         print(f'Error: {error} while connecting to {db_name}')
@@ -34,4 +34,10 @@ date_car = (
 create_bd(db_name=db_name)
 
 for car_info in date_car:
-    add_car_in_bd(db_cars=db_name, date_car=car_info)
+    add_car_in_bd(db_cars=db_name, car_info=car_info)
+
+with sqlite3.connect(db_name) as db_conn:
+    cursor = db_conn.cursor()
+    cursor.execute("SELECT * FROM cars")
+    for result in cursor:
+        print(result)
